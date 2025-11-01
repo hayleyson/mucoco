377c77b067ff9eb3a452fe5b5b867d191ec6ad9b
hayleyson <hayleyson92@gmail.com>
2025-11-01 14:50:26 +0900

    minor

--- new_module/evaluation/evaluate_wandb_post_run_file_based.py
+++ new_module/evaluation/evaluate_wandb_post_run_file_based.py
@@ -29,16 +29,30 @@ if __name__ == "__main__":
     #             task="toxicity",
     #             target_style="nontoxic")
     
-    evaluate_main("",
-            "/data/hyeryung/mixmatch/output_samples/detoxic_r/mask_disc_max_len_12_jigsaw_clsf_data_detoxic_em_max_iter_5_temp_1.0_shuffle_True_block_False_alpha_140.0_beta_1.0_delta_15.0_gamma_0.0_theta_100.0_date_08_06_2024_17_01_54/opt_samples.jsonl",
-            metrics="ppl-qwen",
-            task="toxicity",
-            target_style="nontoxic")
+#     evaluate_main("",
+#             "/data/hyeryung/mixmatch/output_samples/detoxic_r/mask_disc_max_len_12_jigsaw_clsf_data_detoxic_em_max_iter_5_temp_1.0_shuffle_True_block_False_alpha_140.0_beta_1.0_delta_15.0_gamma_0.0_theta_100.0_date_08_06_2024_17_01_54/opt_samples.jsonl",
+#             metrics="ppl-qwen",
+#             task="toxicity",
+#             target_style="nontoxic")
     
-    evaluate_main("",
-            "/data/hyeryung/BOLT/detoxic/detoxic/gen_len20.jsonl",
-            metrics="ppl-qwen",
-            task="toxicity",
-            target_style="nontoxic")
+#     evaluate_main("",
+#             "/data/hyeryung/BOLT/detoxic/detoxic/gen_len20.jsonl",
+#             metrics="ppl-qwen",
+#             task="toxicity",
+#             target_style="nontoxic")
+
+    # evaluate_main(
+    #         "",
+    #         "new_module/data/set_nli/processed_data/set_nli_test.jsonl",
+    #         "set-consistency,dist-n,repetition,fluency",
+    #         task="set_nli",
+    #     )  
+    
+    evaluate_main(
+            "",
+            "new_module/data/convqa/processed_data/lconvqa_test.jsonl",
+            "set-consistency,dist-n,repetition,fluency",
+            task="lconvqa",
+        )  
     
     
