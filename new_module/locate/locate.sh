f856816525be83cb8f39d4b1b5293914cf96afb2
hayleyson <hayleyson92@gmail.com>
2026-01-16 12:16:33 +0900

    minor clean up of new_module/locate

--- new_module/locate/locate.sh
+++ new_module/locate/locate.sh
@@ -3,9 +3,8 @@
 #SBATCH --mem=10GB
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --nodelist=n02
-#SBATCH --gres=gpu:1
-#SBATCH --output='new_module/_slurm_outs/loc_skiml_%j.out'
+#SBATCH --gres=gpu:A6000:1
+#SBATCH --output='new_module/_slurm_outs/loc_locate_edit_%j.out'
 
 source ~/.bashrc
 source ~/miniconda3/etc/profile.d/conda.sh
@@ -13,69 +12,119 @@ conda activate loc-edit
 
 export PYTHONPATH=.
 export HF_HOME=/home/hyeryung/data/hf_cache
-export HF_DATASETS_CACHE=/home/hyeryung/data/hf_cache
-export TRANSFORMERS_CACHE=/home/hyeryung/data/hf_cache
 
-# python new_module/locate/new_locate_utils.py \
-# --pretrained_model_path "/home/hyeryung/data/loc_edit/models/roberta-base-yelp-sentiment-classifier-energy-training/step_81900_best_checkpoint" \
-# --input_file "new_module/data/sentiment/dev_set_below_negative_threshold_827.jsonl" \
-# --output_file "new_module/locate/locate_num_tokens_eda/dev_set_negative_locate_max_5.jsonl" \
-# --task sentiment \
-# --label_id 0
+root_dir="/home/hyeryung/data"
 
-# python new_module/locate/new_locate_utils.py \
-# --pretrained_model_path "/home/hyeryung/data/loc_edit/models/roberta-base-yelp-sentiment-classifier-energy-training/step_81900_best_checkpoint" \
-# --input_file "new_module/data/sentiment/dev_set_below_positive_threshold_778.jsonl" \
-# --output_file "new_module/locate/locate_num_tokens_eda/dev_set_positive_locate_max_5.jsonl" \
-# --task sentiment \
-# --label_id 1
+MAX_NUM_TOKENS=7
 
-# python new_module/locate/new_locate_utils.py \
-# --pretrained_model_path "/home/hyeryung/data/loc_edit/models/roberta-base-jigsaw-toxicity-classifier-energy-training/step_1000_best_checkpoint" \
-# --input_file "new_module/data/toxicity-avoidance/testset_gpt2_2500_below_nontoxic_threshold_468.jsonl" \
-# --output_file "new_module/locate/locate_num_tokens_eda/testset_gpt2_2500_nontoxic_locate_max_5.jsonl" \
+# srun python new_module/locate/new_locate_utils.py \
+# --pretrained_model_path "${root_dir}/loc_edit/models/roberta-base-jigsaw-toxicity-classifier-energy-training/step_1000_best_checkpoint" \
+# --input_file "new_module/data/locate/toxicspans/realtoxicityprompts_gpt2_gen_115_locate_labels_raveled.jsonl" \
+# --output_file "new_module/locate/results/toxicspans/energy_model_gradient_norm_max_num_tokens_${MAX_NUM_TOKENS}.jsonl" \
 # --task toxicity \
-# --label_id 0
+# --label_id 0 \
+# --max_num_tokens $MAX_NUM_TOKENS \
+# --locate_method grad_norm
 
-# python new_module/locate/new_locate_utils.py \
-# --pretrained_model_path "/home/hyeryung/data/loc_edit/models/nli/roberta_large_snli_mnli_anli_train_dev_with_finegrained_finegrained_labels_cross_entropy_n_a/zgs9e2sr/" \
-# --input_file "new_module/data/logical-consistency/anli-r2-test_prompt_4_below_consistent_threshold_3105.jsonl" \
-# --output_file "new_module/locate/locate_num_tokens_eda/anli-r2-test_consistent_locate_max_5.jsonl" \
+# srun python new_module/locate/new_locate_utils.py \
+# --pretrained_model_path "${root_dir}/loc_edit/models/roberta-base-jigsaw-toxicity-classifier-energy-training/step_1000_best_checkpoint" \
+# --input_file "new_module/data/locate/toxicspans/realtoxicityprompts_gpt2_gen_115_locate_labels_raveled.jsonl" \
+# --output_file "new_module/locate/results/toxicspans/energy_model_attention_max_num_tokens_${MAX_NUM_TOKENS}.jsonl" \
+# --task toxicity \
+# --label_id 0 \
+# --max_num_tokens $MAX_NUM_TOKENS \
+# --locate_method attention
+
+# srun python new_module/locate/new_locate_utils.py \
+# --pretrained_model_path "${root_dir}/loc_edit/models/roberta-base-jigsaw-toxicity-classifier/step_500_best_checkpoint/" \
+# --input_file "new_module/data/locate/toxicspans/realtoxicityprompts_gpt2_gen_115_locate_labels_raveled.jsonl" \
+# --output_file "new_module/locate/results/toxicspans/classifier_gradient_norm_max_num_tokens_${MAX_NUM_TOKENS}.jsonl" \
+# --task toxicity \
+# --label_id 0 \
+# --max_num_tokens $MAX_NUM_TOKENS \
+# --locate_method grad_norm
+
+# srun python new_module/locate/new_locate_utils.py \
+# --pretrained_model_path "${root_dir}/loc_edit/models/roberta-base-jigsaw-toxicity-classifier/step_500_best_checkpoint/" \
+# --input_file "new_module/data/locate/toxicspans/realtoxicityprompts_gpt2_gen_115_locate_labels_raveled.jsonl" \
+# --output_file "new_module/locate/results/toxicspans/classifier_attention_max_num_tokens_${MAX_NUM_TOKENS}.jsonl" \
+# --task toxicity \
+# --label_id 0 \
+# --max_num_tokens $MAX_NUM_TOKENS \
+# --locate_method attention
+
+
+
+# srun python new_module/locate/new_locate_utils.py \
+# --pretrained_model_path "${root_dir}/loc_edit/models/nli/roberta_large_snli_mnli_anli_train_dev_with_finegrained_finegrained_labels_cross_entropy_n_a/zgs9e2sr/" \
+# --input_file "new_module/data/locate/inconsistentspans/nli_contra_300_locate_labels_final_raveled.jsonl" \
+# --output_file "new_module/locate/results/inconsistentspans/energy_model_gradient_norm_max_num_tokens_${MAX_NUM_TOKENS}.jsonl" \
 # --task nli \
-# --label_id 1
+# --label_id 0 \
+# --max_num_tokens $MAX_NUM_TOKENS \
+# --locate_method grad_norm
 
-# python new_module/locate/new_locate_utils.py \
-# --pretrained_model_path "/home/hyeryung/data/loc_edit/models/roberta-base-pt16-formality-classifier-energy-training/step_1120_best_checkpoint/" \
-# --input_file "data/formality/GYAFC_Corpus/Entertainment_Music/test/formal" \
-# --output_file "new_module/locate/locate_num_tokens_eda/GYAFC_Corpus_Entertainment_Music_test_informal_locate_max_5.txt" \
-# --task formality \
-# --label_id 0
+# srun python new_module/locate/new_locate_utils.py \
+# --pretrained_model_path "${root_dir}/loc_edit/models/nli/roberta_large_snli_mnli_anli_train_dev_with_finegrained_finegrained_labels_cross_entropy_n_a/zgs9e2sr/" \
+# --input_file "new_module/data/locate/inconsistentspans/nli_contra_300_locate_labels_final_raveled.jsonl" \
+# --output_file "new_module/locate/results/inconsistentspans/energy_model_attention_max_num_tokens_${MAX_NUM_TOKENS}.jsonl" \
+# --task nli \
+# --label_id 0 \
+# --max_num_tokens $MAX_NUM_TOKENS \
+# --locate_method attention
 
-# python new_module/locate/new_locate_utils.py \
-# --pretrained_model_path "/home/hyeryung/data/loc_edit/models/roberta-base-pt16-formality-classifier-energy-training/step_1120_best_checkpoint/" \
-# --input_file "data/formality/GYAFC_Corpus/Entertainment_Music/test/informal" \
-# --output_file "new_module/locate/locate_num_tokens_eda/GYAFC_Corpus_Entertainment_Music_test_formal_locate_max_5.txt" \
-# --task formality \
-# --label_id 1
 
-# python new_module/locate/new_locate_utils.py \
-# --pretrained_model_path "/home/hyeryung/data/loc_edit/models/roberta-base-jigsaw-toxicity-classifier-energy-training/step_1000_best_checkpoint" \
-# --input_file "new_module/llm_experiments/generate_with_llm/baselm_gens/gpt-3.5-turbo-0125/nontoxic/gpt-3.5-turbo-0125_realtoxicityprompts_0shot_150.jsonl" \
-# --output_file "new_module/locate/locate_num_tokens_eda/gpt-3.5-turbo-0125_realtoxicityprompts_0shot_150_nontoxic_locate_max_5.jsonl" \
-# --task toxicity \
-# --label_id 0
+# srun python new_module/locate/new_locate_utils.py \
+# --pretrained_model_path "${root_dir}/loc_edit/models/nli/roberta_large_snli_mnli_anli_train_dev_with_finegrained_binary_labels_binary_cross_entropy_n_a/1731247443/" \
+# --input_file "new_module/data/locate/inconsistentspans/nli_contra_300_locate_labels_final_raveled.jsonl" \
+# --output_file "new_module/locate/results/inconsistentspans/classifier_gradient_norm_max_num_tokens_${MAX_NUM_TOKENS}.jsonl" \
+# --task nli \
+# --label_id 0 \
+# --max_num_tokens $MAX_NUM_TOKENS \
+# --locate_method grad_norm
 
-# python new_module/locate/new_locate_utils.py \
-# --pretrained_model_path "/home/hyeryung/data/loc_edit/models/roberta-base-jigsaw-toxicity-classifier-energy-training/step_1000_best_checkpoint" \
-# --input_file "new_module/llm_experiments/generate_with_llm/baselm_gens/gpt-3.5-turbo-0125/nontoxic/gpt-3.5-turbo-0125_realtoxicityprompts_0shot_150.jsonl" \
-# --output_file "new_module/locate/locate_num_tokens_eda/gpt-3.5-turbo-0125_realtoxicityprompts_0shot_150_nontoxic_locate_max_5_TEST.jsonl" \
-# --task toxicity \
-# --label_id 0
+# srun python new_module/locate/new_locate_utils.py \
+# --pretrained_model_path "${root_dir}/loc_edit/models/nli/roberta_large_snli_mnli_anli_train_dev_with_finegrained_binary_labels_binary_cross_entropy_n_a/1731247443/" \
+# --input_file "new_module/data/locate/inconsistentspans/nli_contra_300_locate_labels_final_raveled.jsonl" \
+# --output_file "new_module/locate/results/inconsistentspans/classifier_attention_max_num_tokens_${MAX_NUM_TOKENS}.jsonl" \
+# --task nli \
+# --label_id 0 \
+# --max_num_tokens $MAX_NUM_TOKENS \
+# --locate_method attention
 
-python new_module/locate/new_locate_utils.py \
---pretrained_model_path "/home/hyeryung/data/loc_edit/models/roberta-base-jigsaw-toxicity-classifier-energy-training/step_1000_best_checkpoint" \
---input_file "new_module/llm_experiments/generate_with_llm/baselm_gens/gpt-3.5-turbo-0125/nontoxic/gpt-3.5-turbo-0125_realtoxicityprompts_0shot_150_below_nontoxic_threshold_0_95_332.jsonl" \
---output_file "new_module/locate/locate_num_tokens_eda/gpt-3.5-turbo-0125_realtoxicityprompts_0shot_150_below_nontoxic_threshold_0_95_332_locate_max_7.jsonl" \
---task toxicity \
---label_id 0
 
+srun python new_module/locate/new_locate_utils.py \
+--pretrained_model_path "${root_dir}/loc_edit/models/roberta-base-jigsaw-toxicity-classifier-energy-training/step_1000_best_checkpoint" \
+--input_file "new_module/data/locate/toxicspans/realtoxicityprompts_gpt2_gen_extended_locate_labels.jsonl" \
+--output_file "new_module/locate/results/toxicspans_extended/energy_model_gradient_norm_max_num_tokens_${MAX_NUM_TOKENS}.jsonl" \
+--task toxicity_extended \
+--label_id 0 \
+--max_num_tokens $MAX_NUM_TOKENS \
+--locate_method grad_norm
+
+srun python new_module/locate/new_locate_utils.py \
+--pretrained_model_path "${root_dir}/loc_edit/models/roberta-base-jigsaw-toxicity-classifier-energy-training/step_1000_best_checkpoint" \
+--input_file "new_module/data/locate/toxicspans/realtoxicityprompts_gpt2_gen_extended_locate_labels.jsonl" \
+--output_file "new_module/locate/results/toxicspans_extended/energy_model_attention_max_num_tokens_${MAX_NUM_TOKENS}.jsonl" \
+--task toxicity_extended \
+--label_id 0 \
+--max_num_tokens $MAX_NUM_TOKENS \
+--locate_method attention
+
+srun python new_module/locate/new_locate_utils.py \
+--pretrained_model_path "${root_dir}/loc_edit/models/roberta-base-jigsaw-toxicity-classifier/step_500_best_checkpoint/" \
+--input_file "new_module/data/locate/toxicspans/realtoxicityprompts_gpt2_gen_extended_locate_labels.jsonl" \
+--output_file "new_module/locate/results/toxicspans_extended/classifier_gradient_norm_max_num_tokens_${MAX_NUM_TOKENS}.jsonl" \
+--task toxicity_extended \
+--label_id 0 \
+--max_num_tokens $MAX_NUM_TOKENS \
+--locate_method grad_norm
+
+srun python new_module/locate/new_locate_utils.py \
+--pretrained_model_path "${root_dir}/loc_edit/models/roberta-base-jigsaw-toxicity-classifier/step_500_best_checkpoint/" \
+--input_file "new_module/data/locate/toxicspans/realtoxicityprompts_gpt2_gen_extended_locate_labels.jsonl" \
+--output_file "new_module/locate/results/toxicspans_extended/classifier_attention_max_num_tokens_${MAX_NUM_TOKENS}.jsonl" \
+--task toxicity_extended \
+--label_id 0 \
+--max_num_tokens $MAX_NUM_TOKENS \
+--locate_method attention
