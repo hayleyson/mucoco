2c685fe0c992ccdcfd125f11fc458ada7e64058a
hayleyson <hayleyson92@gmail.com>
2026-03-05 14:48:13 +0900

    Minor update.

--- new_module/evaluation/evaluate_wandb_post_run.sh
+++ new_module/evaluation/evaluate_wandb_post_run.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem=32GB
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --nodelist=n02
+#SBATCH --nodelist=n01
 #SBATCH --gres=gpu:1
 #SBATCH --output='new_module/_slurm_outs/eval_%j.out'
 
@@ -12,8 +12,8 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate loc-edit
 
 export PYTHONPATH=.
-export HF_HOME=/data/hyeryung/hf_cache
-export HF_DATASETS_CACHE=/data/hyeryung/hf_cache
-export TRANSFORMERS_CACHE=/data/hyeryung/hf_cache
+export HF_HOME=/home/hyeryung/data/hf_cache
+export HF_DATASETS_CACHE=/home/hyeryung/data/hf_cache
+export TRANSFORMERS_CACHE=/home/hyeryung/data/hf_cache
 
 srun python new_module/evaluation/evaluate_wandb_post_run_file_based.py
--- new_module/evaluation/evaluate_wandb_post_run_file_based.py
+++ new_module/evaluation/evaluate_wandb_post_run_file_based.py
@@ -10,64 +10,43 @@ from new_module.evaluation.evaluate_wandb import evaluate_main
 
 if __name__ == "__main__":
     
-    
-    # evaluate_main("",
-    #               "/data/hyeryung/mucoco/new_module/llm_experiments/generate_with_llm/baselm_gens/gpt-3.5-turbo-0125/nontoxic/gpt-3.5-turbo-0125_realtoxicityprompts_noprompt_150.jsonl",
-    #               metrics="ppl-qwen",
-    #               task="toxicity",
-    #               target_style="nontoxic")
-    
-    # evaluate_main("",
-    #               "/data/hyeryung/mucoco/new_module/data/toxicity-avoidance/testset_gpt2_2500.jsonl",
-    #               metrics="toxicity,ppl-qwen,fluency,repetition,dist-n",
-    #               task="toxicity",
-    #               target_style="nontoxic")
-    
-    # evaluate_main("",
-    #             "/data/hyeryung/mucoco/new_module/data/toxicity-avoidance/testset_gpt2_2500.jsonl",
-    #             metrics="ppl-qwen,fluency,repetition,dist-n",
-    #             task="toxicity",
-    #             target_style="nontoxic")
-    
-#     evaluate_main("",
-#             "/data/hyeryung/mixmatch/output_samples/detoxic_r/mask_disc_max_len_12_jigsaw_clsf_data_detoxic_em_max_iter_5_temp_1.0_shuffle_True_block_False_alpha_140.0_beta_1.0_delta_15.0_gamma_0.0_theta_100.0_date_08_06_2024_17_01_54/opt_samples.jsonl",
-#             metrics="ppl-qwen",
-#             task="toxicity",
-#             target_style="nontoxic")
-    
-#     evaluate_main("",
-#             "/data/hyeryung/BOLT/detoxic/detoxic/gen_len20.jsonl",
-#             metrics="ppl-qwen",
-#             task="toxicity",
-#             target_style="nontoxic")
-
     # evaluate_main(
     #         "",
-    #         "new_module/data/set_nli/processed_data/set_nli_test.jsonl",
-    #         "set-consistency,dist-n,repetition,fluency",
-    #         task="set_nli",
+    #         "outputs/sc_energy/set_lconvqa/debug/outputs.txt",
+    #         "set-consistency,ppl-qwen,fluency,dist-n,repetition,contents-preservation",
+    #         task="set_lconvqa",
+    #         source_file_path="/home/hyeryung/data/mucoco/new_module/data/convqa/processed_data/lconvqa_test1_edited_only.jsonl"
     #     )  
-
-    
+#     evaluate_main(
+#             "",
+#             "new_module/data/set_nli/eval2/set_nli_eval2.jsonl",
+#             "set-consistency,ppl-qwen,fluency,dist-n,repetition,contents-preservation",
+#             task="set_snli",
+#             source_file_path="/home/hyeryung/data/mucoco/new_module/data/set_nli/processed_data/set_nli_test1_edited_only.jsonl"
+#     )  
+#     evaluate_main(
+#             "",
+#             "new_module/data/set_nli/eval2/set_nli_eval2.jsonl",
+#             "set-consistency,ppl-qwen,fluency,dist-n,repetition",
+#             task="set_snli",
+#             source_file_path="/home/hyeryung/data/mucoco/new_module/data/set_nli/processed_data/set_nli_test1_edited_only.jsonl"
+#     ) 
     evaluate_main(
             "",
-            "outputs/sc_energy/nli/qjm05u0s/outputs.txt",
-            "set-consistency,ppl-qwen,dist-n,repetition,fluency",
-            task="set_nli",
-        )  
-
-    
-    # evaluate_main(
-    #         "",
-    #         "outputs/sc_energy/nli/r69a1rrf/outputs.txt",
-    #         "set-consistency",
-    #         task="set_nli",
-    #     )      
-    
-    # evaluate_main(
-    #     "",
-    #     "new_module/data/set_nli/processed_data/set_nli_test.jsonl",
-    #     "ppl-qwen",
-    #     task="set_nli",
-    # )  
+            "outputs/sc_energy/set_snli/ebm/btah19b5/outputs.txt",
+            "set-consistency-clsf",
+            task="set_snli",
+    ) 
+    evaluate_main(
+            "",
+            "outputs/sc_energy/set_snli/classifier/b48qkoxt/outputs.txt",
+            "set-consistency-clsf",
+            task="set_snli",
+    ) 
+    evaluate_main(
+            "",
+            "new_module/data/set_nli/processed_data/set_nli_test1_edited_only.jsonl",
+            "set-consistency-clsf",
+            task="set_snli",
+    ) 
         
