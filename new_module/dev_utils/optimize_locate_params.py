31e1e06fea65a07dab3e79c17716d5dcf20ce04f
hayleyson <hayleyson92@gmail.com>
2026-03-27 01:11:56 +0900

    script to grid search for instance localization config.

--- new_module/dev_utils/optimize_locate_params.py
+++ new_module/dev_utils/optimize_locate_params.py
@@ -1,223 +1,142 @@
-from new_module.dev_utils.utils import load_sc_energy_model
-from new_module.locate.new_locate_utils import LocateMachine4SCE
-from new_module.dev_utils.utils import read_outputs
-import yaml 
+import yaml, os, torch, argparse, pickle, sys, json
 from typing import List, Tuple
-import os
 import pandas as pd
-import torch
 from tqdm import tqdm
 import numpy as np
 from copy import deepcopy
 import torch.nn.functional as F
-import argparse
-import pickle
 
-class CustomLocateMachine4SCE(LocateMachine4SCE):
-    def locate_instance(self, prediction: List[str], max_num_tokens: int = 6, unit: str = "word",**kwargs) -> Tuple[List[str], List[List[int]]]:
+from new_module.dev_utils.utils import read_outputs
+from new_module.locate.new_locate_utils import LocateMachine4SCE
+from new_module.set_consistency_locate import locate_ebm, _pkl_path, load_pickle_dataset
 
-        """
-        Locate a instance (a pair) within the input set (set of pairs). 
-        
-        Suppose input text looks like '<s> q1 </s> a1 </s>, q1, ..., </s>'. 
-        The located instance can be anywhere in q1, a2, q2, a2, ...
-        """
-        
-        # logger.debug(f"[new_locate_utils] prediction before adding cls token: {prediction}")
-        prediction = [self.tokenizer.cls_token + " " + p.lstrip(self.tokenizer.cls_token).lstrip(" ") for p in prediction]
-        # logger.debug(f"[new_locate_utils] prediction after adding cls token: {prediction}")
-        outputs, hidden_states_or_attentions = self.energynet.energy_model(prediction, pair_only = True)
-        # Calculate token scores
-        token_scores = self._calculate_token_scores(outputs, hidden_states_or_attentions)
-        
-        # set additional information
-        inputs = self._instance_preserving_encode_plus(prediction)
-        input_tensor = inputs['input_ids']
-        mask = inputs['attention_mask']
-        # logger.debug(f"input_tensor: {input_tensor}")
-        # logger.debug(f"mask: {mask}")
-        
-        _, instance_locations = self._detect_instance(prediction) 
-           
-        batch_size = input_tensor.shape[0]
-        assert batch_size == 1 # this code assumes batch_size = 1
-            
-        # initialize return variables
-        prediction_list = []
-        masked_sequence_text = []
-        
-        # Apply attention and stopwords mask. Then take softmax
-        final_mask = (mask == 0) | torch.isin(input_tensor, self.stopwords_ids)
-        token_scores[final_mask] = -float("inf")
-        token_scores = token_scores.softmax(dim=-1)
-        
-        # Filter out degenerate instances (those with only masked tokens)
-        # This prevents division by zero errors in instance scoring
-        filtered_instance_locations = []
-        filtered_instance_indexes = []
-        for b in range(batch_size):
-            valid_instances = []
-            for j, (start, end) in enumerate(instance_locations[b]):
-                # Check if instance has at least one non-masked token
-                instance_has_nonmasked = (~final_mask[b][start:end]).any().item()
-                if instance_has_nonmasked:
-                    valid_instances.append((start, end))
-                    filtered_instance_indexes.append(j)
-                else:
-                    logger.info(f"Filtering out degenerate instance at ({start}, {end}) with only masked tokens")
-            filtered_instance_locations.append(valid_instances)
-        
-        # Update instance_locations to use only valid instances
-        instance_locations = filtered_instance_locations
-        
-        # First locate at instance-level
-        prediction_list = self._locate_instance(token_scores, instance_locations, batch_size)
-        prediction_list_adjusted = [filtered_instance_indexes[prediction_list[0][0]]]
+sys.path.append("new_module/set_consistency_energy")
+from energynets.energynet import energynet
+from tasks.dataset_loader import concat_arbitrary_pairs
+from energynets.decomposition.no_decomposition import no_decomposition_loader
 
-        # Create text with the located instance removed
-        predicted_instance_start, predicted_instance_end = instance_locations[0][prediction_list[0][0]]
+
         
-        new_input_tensor = torch.cat([input_tensor[:, :predicted_instance_start], input_tensor[:, predicted_instance_end:]], axis=-1) if predicted_instance_start > 0 else input_tensor[:, predicted_instance_end:]
-        new_prediction = self.tokenizer.batch_decode(new_input_tensor)
-        new_prediction = new_prediction[0].strip('<s>').strip(' ')
-
-        predicted_instance_tensor = input_tensor[:, predicted_instance_start: predicted_instance_end]
-        predicted_instance = self.tokenizer.batch_decode(predicted_instance_tensor)
-
-        return new_prediction,predicted_instance, prediction_list_adjusted
+def load_sc_energy_model_customized(config_path, device, locate_instance_type = ""):
     
+    model_config = yaml.load(open(config_path), 
+                                Loader=yaml.FullLoader)
+    model_config['device'] = device
+
+    if locate_instance_type != "":
+        model_config['locate']['instance']['type'] = locate_instance_type
+
+    energy_net = energynet(params=model_config)
+    energy_net.load_state_dict(torch.load(model_config["model_path"], 
+                                        map_location=model_config['device'],
+                                        weights_only=True)['state_dict'], strict=False)
+    if 'threshold' in torch.load(model_config["model_path"],
+                                map_location=model_config['device'],
+                                weights_only=True):
+        energy_net.threshold = torch.load(model_config["model_path"],
+                                map_location=model_config['device'],
+                                weights_only=True)['threshold']
+    energy_net.eval()
+    energy_net.to(device)
     
-    def verify_consistency(self, text):
-        
+    return energy_net
+    
+def load_eval2_dataset(dataset_name):
+    # 규칙에 따라 Eval2 데이터셋을 로딩한다.
+    eval2_con_dataset_arbitrary_pairs = load_pickle_dataset(dataset_name, "eval2", "C")
+    eval2_incon_dataset_arbitrary_pairs = load_pickle_dataset(dataset_name, "eval2", "I")
+    eval2_con_dataset_arbitrary_pairs.dataset = [t for t in eval2_con_dataset_arbitrary_pairs.dataset if len(t) >=4]
+    eval2_incon_dataset_arbitrary_pairs.dataset = [t for t in eval2_incon_dataset_arbitrary_pairs.dataset if len(t) >=4]
+    
+    concat2_dataset, concat2_names, concat2_set_sizes = concat_arbitrary_pairs([eval2_con_dataset_arbitrary_pairs, eval2_incon_dataset_arbitrary_pairs], concat_num=2)
+    concat3_dataset, concat3_names, concat3_set_sizes = concat_arbitrary_pairs([eval2_con_dataset_arbitrary_pairs, eval2_incon_dataset_arbitrary_pairs], concat_num=3)
+    concat4_dataset, concat4_names, concat4_set_sizes = concat_arbitrary_pairs([eval2_con_dataset_arbitrary_pairs, eval2_incon_dataset_arbitrary_pairs], concat_num=4)
 
-        with torch.no_grad():
-            # set consistency verification
-            batch_text = ['<s> ' + text]
-            output, _ = self.energynet.energy_model(batch_text, pair_only = True)
-            
-            if (self.energynet.output_form == 'real_num'):
-                probs = output.reshape(-1)
-            elif (self.energynet.output_form == '2dim_vec'):
-                probs = F.softmax(output, dim=-1)[:,-1]
-            else:
-                raise ValueError(f"Unsupported output form: {self.energynet.output_form}")
-        
-            #  classify
+    eval2_steps_names = ['con', 'incon'] + concat2_names+ concat3_names+ concat4_names
+    eval2_datasets = [eval2_con_dataset_arbitrary_pairs, eval2_incon_dataset_arbitrary_pairs
+            ] + concat2_dataset + concat3_dataset + concat4_dataset
 
-            cons = torch.where(probs <= self.energynet.threshold,1,0).item()
-        if cons == 1:
-            return 'con'
-        else:
-            return 'incon'
+    # Classification accuracy와 별개로 locate accuracy만 보고 싶기 때문에, incon인 샘플만 취한다.
+    eval2_datasets = [eval2_datasets[i] for i in range(len(eval2_datasets)) if ('incon' in eval2_steps_names[i])]
+    eval2_steps_names = [eval2_steps_name for eval2_steps_name in eval2_steps_names if ('incon' in eval2_steps_name)]
+    print(f"Using samples from inconsistent datasets: {eval2_steps_names}")
+    
+    # 편의상 데이터셋을 하나로 합친다.
+    eval2_samples = []
+    for dataset in eval2_datasets:
+        eval2_samples.extend(dataset.dataset)
+    print(f"Num samples: {len(eval2_samples)}")
+    
+    canonical_eval2_dataset = eval2_datasets[0]
+    canonical_eval2_dataset.dataset = eval2_samples
 
-    def detect_spans_recursive_max(self, text):
-        predicted_indexes = []
-        remaining_index_list = list(range(len(self.detect_span(text))))
-
-        while ((self.verify_consistency(text) == 'incon') and len(text) > 0):
-            
-            text,_, index_list = self.locate_instance([text])
-            index = index_list[0]
-            
-            predicted_indexes.append(remaining_index_list[index])
-            remaining_index_list = (remaining_index_list[:index] if index > 0 else []) + remaining_index_list[index+1:] 
-        
-        return predicted_indexes
+    return canonical_eval2_dataset
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--task', type=str, required=True)
-    parser.add_argument('--config', type=str, required=True)
-    parser.add_argument('--data_path', type=str, required=True)
+    parser.add_argument('--params_path', type=str, required=True)
     parser.add_argument('--output_dir', type=str, required=True)
     parser.add_argument('--device', type=str, default='cuda')
-    parser.add_argument('--agg_method', type=str, default='')
-    parser.add_argument('--attentions_num_layer', type=int, default=-1)
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    config = yaml.load(open(args.config), Loader=yaml.FullLoader)
-    config['device'] = args.device
-    model = load_sc_energy_model(args.config, args.device)
-
-    if args.agg_method != '':
-        config['locate']['agg_method'] = args.agg_method
-        print(f"Overrode agg_method to {args.agg_method}.")
-    locator = CustomLocateMachine4SCE(config, model, args.task)
     
-    data = read_outputs(args.data_path)
-    incon_data = data.loc[data['label'] == 'incon'].copy()
-    
-    metric_store = []
+    # ------------------------------------------------ #
+    # Data Loading
+    # ------------------------------------------------ #
+    # 데이터셋을 정의한다.
+    canonical_eval2_dataset = load_eval2_dataset(dataset_name = "lconvqa")
 
-    if config['locate']['type'] == 'attention':
+    # ------------------------------------------------ #
+    # Initialize EBM & Conduct Locate
+    # ------------------------------------------------ #
+    # locate 관련된 것 이외의 parameter는 default값을 불러와 바꾸지 않고 사용한다.
+    params_ebm = yaml.load(open(args.params_path), Loader=yaml.FullLoader)
+    params_ebm['device'] = args.device
 
-        if args.attentions_num_layer != -1:
-            num_layer_list = [args.attentions_num_layer]
-        else:
-            num_layer_list = list(range(12))
-
-        for num_layer in tqdm(num_layer_list):
-
-            locator.params['locate']['attentions_num_layer'] = num_layer
-            
-            precisions = []
-            recalls = []
-            for ix, row in tqdm(incon_data.iterrows()):
-                predicted_locate_labels = locator.detect_spans_recursive_max(row['text'])
-                gold_locate_labels = row['locate_labels']
-                if len(predicted_locate_labels) == 0:
-                    precisions.append(0)
-                    continue
-                precision = len(set(predicted_locate_labels) & set(gold_locate_labels)) / len(predicted_locate_labels)
-                precisions.append(precision)
-
-                recall = len(set(predicted_locate_labels) & set(gold_locate_labels)) / len(gold_locate_labels)
-                recalls.append(recall)
-            
-            metric_store_ = deepcopy(locator.params['locate'])
-            metric_store_['precision'] = np.mean(precisions)
-
-            metric_store_['recall'] = np.mean(recalls)
-            metric_store.append(metric_store_)
-            print(f'Layer {num_layer}: Average Precision = {np.mean(precisions)}, Average Recall = {np.mean(recalls)}')
-
-    elif config['locate']['type'] == 'gradnorm':
-
-        precisions = []
-        recalls = []
-        for ix, row in tqdm(incon_data.iterrows()):
-            predicted_locate_labels = locator.detect_spans_recursive_max(row['text'])
-            gold_locate_labels = row['locate_labels']
-            if len(predicted_locate_labels) == 0:
-                precisions.append(0)
-                continue
-            
-            precision = len(set(predicted_locate_labels) & set(gold_locate_labels)) / len(predicted_locate_labels)
-            precisions.append(precision)
-
-            recall = len(set(predicted_locate_labels) & set(gold_locate_labels)) / len(gold_locate_labels)
-            recalls.append(recall)
+    # Attention 모든 layer에 대해서, gradient norm에 대해서 총 13가지 케이스에 대해서 평가 실시한다.
+    results = {}
+    for locate_instance_type in ['attention', 'gradnorm']:
         
-        metric_store_ = deepcopy(locator.params['locate'])
-        metric_store_['precision'] = np.mean(precisions)
-        metric_store_['recall'] = np.mean(recalls)
-        metric_store.append(metric_store_)
-        print(f'Average Precision = {np.mean(precisions)}, Average Recall = {np.mean(recalls)}')
+        energynet = load_sc_energy_model_customized(args.params_path, args.device, locate_instance_type)
+        locator = LocateMachine4SCE(params_ebm, energynet, "lconvqa")
 
-    if os.path.exists(args.output_dir + '/locate_params_eval_result.csv'):
-        print(f"{args.output_dir + '/locate_params_eval_result.csv'} exists.")
-        print('Loading from existing eval result file...')
-        prev_metric_store = pd.read_csv(args.output_dir + '/locate_params_eval_result.csv')
-        print(f"Loaded {len(prev_metric_store)} results.")
-        metric_store = pd.concat([prev_metric_store, pd.DataFrame.from_dict(metric_store)], axis=0, ignore_index=True)
-    else:
-        metric_store = pd.DataFrame.from_dict(metric_store)
-        
-    metric_store.to_csv(args.output_dir + '/locate_params_eval_result.csv', index=False)
-    print(f"Eval result saved to {args.output_dir + '/locate_params_eval_result.csv'}")
+        # 데이터 로더를 정의한다.
+        data_loader = no_decomposition_loader(canonical_eval2_dataset, tokenizer=energynet.representation_model.tokenizer, params=params_ebm).get_loader()
+
+        for agg_method in ['avg', 'median']:
+
+            locator.params['locate']['instance']['agg_method'] = agg_method
+
+            if locate_instance_type == 'attention':
+                for num_layer in range(12):
+                    
+                    locator.params['locate']['instance']['attentions_num_layer'] = num_layer
+                    
+                    result = locate_ebm(locator, data_loader, device=params_ebm['device'], params=None)
+
+                    results[f"precision_{locate_instance_type}_{agg_method}_{num_layer}"] = result['precision']
+                    results[f"recall_{locate_instance_type}_{agg_method}_{num_layer}"] = result['recall']
+                    results[f"f1_{locate_instance_type}_{agg_method}_{num_layer}"] = result['f1']
+                    results[f"accuracy_{locate_instance_type}_{agg_method}_{num_layer}"] = result['accuracy']
+
+                    print(f'Type {locate_instance_type}, Agg {agg_method}, Layer {num_layer}: Precision = {result["precision"]}, Recall = {result["recall"]}, F1 = {result["f1"]}, Accuracy = {result["accuracy"]}')
+
+            elif locate_instance_type == 'gradnorm':
+                
+                result = locate_ebm(locator, data_loader, device=params_ebm['device'], params=None)
+
+                results[f"precision_{locate_instance_type}_{agg_method}"] = result['precision']
+                results[f"recall_{locate_instance_type}_{agg_method}"] = result['recall']
+                results[f"f1_{locate_instance_type}_{agg_method}"] = result['f1']
+                results[f"accuracy_{locate_instance_type}_{agg_method}"] = result['accuracy']
+
+                print(f'Type {locate_instance_type}, Agg {agg_method}: Precision = {result["precision"]}, Recall = {result["recall"]}, F1 = {result["f1"]}, Accuracy = {result["accuracy"]}')
+
+    with open(os.path.join(args.output_dir, f'locate_params_eval_result.jsonl'), 'w') as f:
+        json.dump(results, f, indent=4)
 
 if __name__ == '__main__':
     main()
---
+++ new_module/dev_utils/optimize_locate_params.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-12:00:00
+#SBATCH --mem=32GB
+#SBATCH --gres=gpu:1
+#SBATCH --nodelist=n01
+#SBATCH --output='new_module/_slurm_outs/optimize_locate_params_%j.out'
+
+source ~/.bashrc
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate loc-edit
+
+export PYTHONPATH=.
+export HF_HOME=/home/hyeryung/data/hf_cache
+export HF_DATASETS_CACHE=/home/hyeryung/data/hf_cache
+export TRANSFORMERS_CACHE=/home/hyeryung/data/hf_cache
+
+srun -n 1 -c 1 python new_module/dev_utils/optimize_locate_params.py \
+--output_dir outputs/sc_energy/set_lconvqa/locate/eval2set_all \
+--params_path new_module/set_consistency_energy/params_set_lconvqa.yaml
